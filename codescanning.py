8b26041e421bc82f653128949d0ff4bb976b68eb
Gee-tha-S <166114149+Gee-tha-S@users.noreply.github.com>
2024-05-15 01:41:23 +0530

    Update codescanning.py

--- codescanning.py
+++ codescanning.py
@@ -4,7 +4,7 @@ import os
 
 # Fetch GitHub Personal Access Token from environment variable
 #TOKEN = "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"
-TOKEN = os.getenv("TOKEN-GITHUB")
+TOKEN = os.getenv("TOKEN_GITHUB")
 print(TOKEN),
 ORG_NAME = "FedEx"
 params = {'per_page': 100}
